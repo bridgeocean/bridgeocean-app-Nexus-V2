a72688255ec20bb707731755df49e650d4388c24
bridgeocean <149322598+bridgeocean@users.noreply.github.com>
2025-06-13 23:56:45 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has TypeScript errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   },
+  experimental: {
+    typedRoutes: false,
+  },
+  // Completely disable type checking
+  swcMinify: true,
 }
 
 export default nextConfig
